8dd9469046d793a502fce5848e522dd32d31e206
Jakub Berezowski <jakub.berezowski@bccgroup.com>
2018-01-14 23:35:55 +0100

    ProduktDAO not tested at all

--- src/sample/model/Produkt.java
+++ src/sample/model/Produkt.java
@@ -2,7 +2,45 @@ package sample.model;
 
 public class Produkt {
     private int id;
-    private int cena;
+    private double cena;
     private String nazwa;
     private RozmiarPorcji rozmiarPorcji;
+
+    public Produkt(double cena, String nazwa, RozmiarPorcji rozmiarPorcji) {
+        this.cena = cena;
+        this.nazwa = nazwa;
+        this.rozmiarPorcji = rozmiarPorcji;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setCena(double cena) {
+        this.cena = cena;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public RozmiarPorcji getRozmiarPorcji() {
+        return rozmiarPorcji;
+    }
+
+    public void setRozmiarPorcji(RozmiarPorcji rozmiarPorcji) {
+        this.rozmiarPorcji = rozmiarPorcji;
+    }
 }
